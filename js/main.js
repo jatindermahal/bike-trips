65790f4aea47fc4a205dd1b01495a9ab46a92d8d
Jatinderpal Singh <jatindermahal11@gmail.com>
2022-06-16 21:58:21 -0400

    main.js updated

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-/********************************************************************************* 
-* WEB422 – Assignment 2 
-*
-* I declare that this assignment is my own work in accordance with Seneca Academic Policy. 
-* No part of this assignment has been copied manually or electronically from any other source 
-* (including web sites) or distributed to other students. 
-* 
-* Name: Jatinderpal Singh Student ID: 155892201 Date: 5/21/22 
-* 
-********************************************************************************/
-
 let tripData = [];
 let currentTrip = {};
 let page = 1;
